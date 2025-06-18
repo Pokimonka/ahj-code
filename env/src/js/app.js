393557e189047052bb7881abdde0471654b85a6d
Pokimonka <alenkaponomareva7@gmail.com>
2025-06-19 01:00:01 +0300

    from lesson

--- env/src/js/app.js
+++ env/src/js/app.js
@@ -1,5 +1,7 @@
 // TODO: write code here
 
+import Example from "./Example";
+
 // comment this to pass build
 const unusedVariable = "variable";
 
@@ -9,3 +11,13 @@ export default function demo(value) {
 }
 
 console.log("app.js included");
+
+const root = document.querySelector(".root");
+
+setTimeout(() => {
+  root.textContent = "content";
+}, 4000);
+
+let exm = new Example(root);
+
+exm.change("text from change");
